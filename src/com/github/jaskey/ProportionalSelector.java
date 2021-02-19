9c63126cd8effd2e1b2233dfa535f7512d5a14ff
zhanggang <zhanggang@reconova.com>
2021-02-19 11:16:00 +0800

    1

--- src/com/github/jaskey/ProportionalSelector.java
+++ src/com/github/jaskey/ProportionalSelector.java
@@ -1,6 +1,9 @@
 package com.github.jaskey;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
